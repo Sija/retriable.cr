8704fbcad7cdf43d79396379a90cbe5a1937c7a3
Sijawusz Pur Rahnama <sija@sija.pl>
2018-02-17 16:45:50 +0100

    Initial commit

---
+++ src/retriable.cr
@@ -0,0 +1,6 @@
+require "./retriable/*"
+
+# TODO: Write documentation for `Retriable`
+module Retriable
+  # TODO: Put your code here
+end
---
+++ src/retriable/version.cr
@@ -0,0 +1,3 @@
+module Retriable
+  VERSION = "0.1.0"
+end
